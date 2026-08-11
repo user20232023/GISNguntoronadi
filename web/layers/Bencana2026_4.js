b13b3d86a6d84f09251c9c4295216ccb38230f56
user20232023 <128923590+user20232023@users.noreply.github.com>
2026-08-11 07:41:20 +0700

    rev 08 02

---
+++ web/layers/Bencana2026_4.js
@@ -0,0 +1,1 @@
+var json_Bencana2026_4 = {"type":"FeatureCollection","name":"Bencana2026_4","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"id":"1"},"geometry":{"type":"Point","coordinates":[110.955368012639767,-7.922741493413856]}}]}
---
+++ web/layers/layers.js
@@ -0,0 +1,105 @@
+var wms_layers = [];
+
+
+        var lyr_OSMStandard_0 = new ol.layer.Tile({
+            'title': 'OSM Standard',
+            'opacity': 1.000000,
+            
+            
+            source: new ol.source.XYZ({
+            attributions: '<a href="https://www.openstreetmap.org/copyright">© OpenStreetMap contributors, CC-BY-SA</a>',
+                url: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png'
+            })
+        });
+
+        var lyr_GoogleSatellite_1 = new ol.layer.Tile({
+            'title': 'Google Satellite',
+            'opacity': 1.000000,
+            
+            
+            source: new ol.source.XYZ({
+            attributions: ' ',
+                url: 'https://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}'
+            })
+        });
+var lyr_NDVI_Kemarau_Nguntoronadi_2 = new ol.layer.Image({
+        opacity: 1,
+        
+    title: 'NDVI_Kemarau_Nguntoronadi<br />\
+    <img src="styles/legend/NDVI_Kemarau_Nguntoronadi_2_0.png" /> -0,1000<br />\
+    <img src="styles/legend/NDVI_Kemarau_Nguntoronadi_2_1.png" /> 0,1250<br />\
+    <img src="styles/legend/NDVI_Kemarau_Nguntoronadi_2_2.png" /> 0,3500<br />\
+    <img src="styles/legend/NDVI_Kemarau_Nguntoronadi_2_3.png" /> 0,5750<br />\
+    <img src="styles/legend/NDVI_Kemarau_Nguntoronadi_2_4.png" /> 0,8000<br />' ,
+        
+        
+        source: new ol.source.ImageStatic({
+            url: "./layers/NDVI_Kemarau_Nguntoronadi_2.png",
+            attributions: ' ',
+            projection: 'EPSG:3857',
+            alwaysInRange: true,
+            imageExtent: [12344190.000000, -887904.051335, 12359410.000000, -874860.492975]
+        })
+    });
+var lyr_NDWI_Risiko_Genangan_Nguntoronadi_3 = new ol.layer.Image({
+        opacity: 1,
+        
+    title: 'NDWI_Risiko_Genangan_Nguntoronadi<br />\
+    <img src="styles/legend/NDWI_Risiko_Genangan_Nguntoronadi_3_0.png" /> -0,8360<br />\
+    <img src="styles/legend/NDWI_Risiko_Genangan_Nguntoronadi_3_1.png" /> 0,0336<br />\
+    <img src="styles/legend/NDWI_Risiko_Genangan_Nguntoronadi_3_2.png" /> 0,2510<br />\
+    <img src="styles/legend/NDWI_Risiko_Genangan_Nguntoronadi_3_3.png" /> 0,4684<br />\
+    <img src="styles/legend/NDWI_Risiko_Genangan_Nguntoronadi_3_4.png" /> 0,6691<br />\
+    <img src="styles/legend/NDWI_Risiko_Genangan_Nguntoronadi_3_5.png" /> 0,8363<br />' ,
+        
+        
+        source: new ol.source.ImageStatic({
+            url: "./layers/NDWI_Risiko_Genangan_Nguntoronadi_3.png",
+            attributions: ' ',
+            projection: 'EPSG:3857',
+            alwaysInRange: true,
+            imageExtent: [12344190.000000, -887904.051335, 12359410.000000, -874860.492975]
+        })
+    });
+var format_Bencana2026_4 = new ol.format.GeoJSON();
+var features_Bencana2026_4 = format_Bencana2026_4.readFeatures(json_Bencana2026_4, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_Bencana2026_4 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_Bencana2026_4.addFeatures(features_Bencana2026_4);
+var lyr_Bencana2026_4 = new ol.layer.Vector({
+                declutter: false,
+                source:jsonSource_Bencana2026_4, 
+                style: style_Bencana2026_4,
+                popuplayertitle: 'Bencana 2026',
+                interactive: true,
+                title: '<img src="styles/legend/Bencana2026_4.png" /> Bencana 2026'
+            });
+var format_batas_wilayah_nguntoronadi_5 = new ol.format.GeoJSON();
+var features_batas_wilayah_nguntoronadi_5 = format_batas_wilayah_nguntoronadi_5.readFeatures(json_batas_wilayah_nguntoronadi_5, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_batas_wilayah_nguntoronadi_5 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_batas_wilayah_nguntoronadi_5.addFeatures(features_batas_wilayah_nguntoronadi_5);
+var lyr_batas_wilayah_nguntoronadi_5 = new ol.layer.Vector({
+                declutter: false,
+                source:jsonSource_batas_wilayah_nguntoronadi_5, 
+                style: style_batas_wilayah_nguntoronadi_5,
+                popuplayertitle: 'batas_wilayah_nguntoronadi',
+                interactive: true,
+                title: '<img src="styles/legend/batas_wilayah_nguntoronadi_5.png" /> batas_wilayah_nguntoronadi'
+            });
+
+lyr_OSMStandard_0.setVisible(true);lyr_GoogleSatellite_1.setVisible(true);lyr_NDVI_Kemarau_Nguntoronadi_2.setVisible(true);lyr_NDWI_Risiko_Genangan_Nguntoronadi_3.setVisible(true);lyr_Bencana2026_4.setVisible(true);lyr_batas_wilayah_nguntoronadi_5.setVisible(true);
+var layersList = [lyr_OSMStandard_0,lyr_GoogleSatellite_1,lyr_NDVI_Kemarau_Nguntoronadi_2,lyr_NDWI_Risiko_Genangan_Nguntoronadi_3,lyr_Bencana2026_4,lyr_batas_wilayah_nguntoronadi_5];
+lyr_Bencana2026_4.set('fieldAliases', {'id': 'id', });
+lyr_batas_wilayah_nguntoronadi_5.set('fieldAliases', {'id': 'id', 'FCODE': 'FCODE', 'KDBBPS': 'KDBBPS', 'KDCBPS': 'KDCBPS', 'KDCPUM': 'KDCPUM', 'KDEBPS': 'KDEBPS', 'KDEPUM': 'KDEPUM', 'KDPBPS': 'KDPBPS', 'KDPKAB': 'KDPKAB', 'KDPPUM': 'KDPPUM', 'LUAS': 'LUAS', 'LUASWH': 'LUASWH', 'METADATA': 'METADATA', 'NAMOBJ': 'NAMOBJ', 'OBJECTID': 'OBJECTID', 'REMARK': 'REMARK', 'SRS_ID': 'SRS_ID', 'TIPADM': 'TIPADM', 'UUPP': 'UUPP', 'WADMKC': 'WADMKC', 'WADMKD': 'WADMKD', 'WADMKK': 'WADMKK', 'WADMPR': 'WADMPR', 'WIADKC': 'WIADKC', 'WIADKD': 'WIADKD', 'WIADKK': 'WIADKK', 'WIADPR': 'WIADPR', });
+lyr_Bencana2026_4.set('fieldImages', {'id': '', });
+lyr_batas_wilayah_nguntoronadi_5.set('fieldImages', {'id': 'TextEdit', 'FCODE': 'TextEdit', 'KDBBPS': 'TextEdit', 'KDCBPS': 'TextEdit', 'KDCPUM': 'TextEdit', 'KDEBPS': 'TextEdit', 'KDEPUM': 'TextEdit', 'KDPBPS': 'TextEdit', 'KDPKAB': 'TextEdit', 'KDPPUM': 'TextEdit', 'LUAS': 'TextEdit', 'LUASWH': 'TextEdit', 'METADATA': 'TextEdit', 'NAMOBJ': 'TextEdit', 'OBJECTID': 'Range', 'REMARK': 'TextEdit', 'SRS_ID': 'TextEdit', 'TIPADM': 'Range', 'UUPP': 'TextEdit', 'WADMKC': 'TextEdit', 'WADMKD': 'TextEdit', 'WADMKK': 'TextEdit', 'WADMPR': 'TextEdit', 'WIADKC': 'TextEdit', 'WIADKD': 'TextEdit', 'WIADKK': 'TextEdit', 'WIADPR': 'TextEdit', });
+lyr_Bencana2026_4.set('fieldLabels', {'id': 'no label', });
+lyr_batas_wilayah_nguntoronadi_5.set('fieldLabels', {'id': 'no label', 'FCODE': 'header label - visible with data', 'KDBBPS': 'no label', 'KDCBPS': 'no label', 'KDCPUM': 'no label', 'KDEBPS': 'no label', 'KDEPUM': 'header label - visible with data', 'KDPBPS': 'no label', 'KDPKAB': 'no label', 'KDPPUM': 'no label', 'LUAS': 'no label', 'LUASWH': 'no label', 'METADATA': 'no label', 'NAMOBJ': 'no label', 'OBJECTID': 'no label', 'REMARK': 'no label', 'SRS_ID': 'no label', 'TIPADM': 'no label', 'UUPP': 'no label', 'WADMKC': 'no label', 'WADMKD': 'no label', 'WADMKK': 'no label', 'WADMPR': 'no label', 'WIADKC': 'no label', 'WIADKD': 'no label', 'WIADKK': 'no label', 'WIADPR': 'no label', });
+lyr_batas_wilayah_nguntoronadi_5.on('precompose', function(evt) {
+    evt.context.globalCompositeOperation = 'normal';
+});
